ed9ca1476166cb6b521d2bd2127a155df0b0f3e2
Anri Lombard <anri.m.lombard@gmail.com>
2021-10-08 20:57:38 +0200

    K clustering solved

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -24,12 +24,12 @@ idx = zeros(size(X,1), 1);
 m = size(X,1);
 
 for i = 1:m
-    distance_array = zeros(1, K);
-    for j = 1:k
-        distance_array(1, j) = sqrt(sum(power((X(i,"")-centroids(j,:)), 2)));
+    distance_array = zeros(1,K);
+    for j = 1:K
+        distance_array(1,j) = sqrt(sum(power((X(i,:)-centroids(j,:)),2)));
     end
-    [~, idx] = min(distance_array);
-    idx(i, 1) = d_idx;
+    [~, d_idx] = min(distance_array);
+    idx(i,1) = d_idx;
 end
 
 
